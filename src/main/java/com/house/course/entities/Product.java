f5a05f60187a970e99c52da621c6052db6281471
Mauricio Frasson Junior <m.frasson.jr@gmail.com>
2019-09-19 21:24:04 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/house/course/entities/Product.java
+++ src/main/java/com/house/course/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -30,8 +33,10 @@ public class Product implements Serializable {
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
-	public Product() {
-	}
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	public Product() { }
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
@@ -85,6 +90,15 @@ public class Product implements Serializable {
 		return categories;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for( OrderItem x : items ) 
+			set.add(x.getOrder());
+		
+		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
